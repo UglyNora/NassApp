16061ad630a0fb9321eab6fac2d1bb8fe0a4c946
Dextarius <dsicarius@gmail.com>
2019-02-21 13:06:18 -0500

    Developed the architecture for the engine and UI. Created classes that represent both the engine itself and the interface to interact with them.
    The majority of the methods have been outlined as stubs and the ones needing work have been marked with a TODO tag and a comment describing the methods intended function.

---
+++ src/com/search_engine/nassapp/SearchWindow.java
@@ -0,0 +1,179 @@
+package com.search_engine.nassapp;
+
+//A standalone GUI search engine app- stub user interface.
+//Written by Mac Wiener, Valrico FL, 2019
+
+
+import javafx.scene.control.Control;
+import javafx.scene.control.RadioButton;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
+
+public class SearchWindow extends JFrame
+{
+    private SearchEngine currentEngine;
+    private JFrame       searchWindowFrame;
+    private JLabel       windowHeaderLabel;
+    private JTextField   searchField;
+    private JButton      searchButton;
+    private JButton      adminButton;
+    private ButtonGroup  searchOptionGroup;
+    private JRadioButton phraseRadioButton;
+    private JRadioButton andRadioButton;
+    private JRadioButton orRadioButton;
+    private JRadioButton notRadioButton;
+    private Container    container;
+
+
+
+    public static void main ( String args[])
+    {
+
+        SearchWindow searchWindow = new SearchWindow();
+
+        return;
+    }
+
+    //TODO :  This method should center a given control vertically within the given Dimension
+    private void CenterControlVertically(Control controlToCenter, Dimension areaSize)
+    {
+
+    }
+    //TODO :  This method should center a given control horizontally within the given Dimension
+    private void CenterControlHorizontally(Control controlToCenter, Dimension areaSize)
+    {
+
+    }
+
+    //TODO :  This method should call all of the other methods concerned with setting up the initial
+    //- display for the window in a reasonable order;
+    private void SetWindowDisplay()
+    {
+
+    }
+
+    //TODO :  This method should assign the 4 search option radio buttons to the searchOptionGroup and
+    //- then configure them so they are visible, are positioned on the page somewhere appropriate,
+    //- and any relevant properties have been set (i.e. height, width, etc)
+    private void ConfigureSearchOptionRadioGroup()
+    {
+
+    }
+
+    //TODO :  This method should take the JButtons and configure them so they are visible, are positioned on
+    //- the page somewhere appropriate, and any relevant properties have been set (i.e. height, width, etc)
+    private void ConfigureButtons()
+    {
+
+    }
+
+    //TODO :  This method should take the remaining controls (searchbox, labels, etc) and configure them so they
+    //- are visible, are positioned on the page somewhere appropriate, and any relevant properties have been
+    //- set (i.e. height, width, etc)
+    private void ConfigureMisc()
+    {
+
+    }
+
+    //TODO : This method should assign the event listeners declared in the Event Handlers section to their
+    //- appropriate controls
+    private void SetEventListeners()
+    {
+
+    }
+
+
+    public SearchWindow()
+    {
+        searchWindowFrame = new JFrame ("NASS Engine");
+        windowHeaderLabel = new JLabel ("NASS");
+        searchField       = new JTextField();
+        searchButton      = new JButton ("Search");
+        adminButton       = new JButton ("Admin");
+        searchOptionGroup = new ButtonGroup();
+        phraseRadioButton = new JRadioButton("Phrase");
+        andRadioButton    = new JRadioButton("And");
+        orRadioButton     = new JRadioButton("Or");
+        notRadioButton    = new JRadioButton("Not");
+
+        container = searchWindowFrame.getContentPane();
+        container.setLayout(new FlowLayout());
+
+        container.add(windowHeaderLabel);
+
+        searchWindowFrame.setSize(1200, 700);
+        searchWindowFrame.setVisible(true);
+        searchWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        currentEngine = new SearchEngine();
+    }
+
+    //|SubClasses|\\
+
+
+
+    //|Event Listeners|\\
+
+    //- Handles the events for the Add File Button
+    private class SearchButtonListener implements ActionListener
+    {
+        SearchWindow windowContainingTriggeredControl;
+
+        //TODO : This method should get the query typed into the searchbox and which of the search option radio
+        //- buttons is selected, and then pass that information to the SearchDatabase method of the current engine
+        @Override public void actionPerformed(ActionEvent e)
+        {
+
+        }
+
+        public SearchButtonListener(SearchWindow parentWindow)
+        {
+            windowContainingTriggeredControl = parentWindow;
+            return;
+        }
+    }
+
+    //- Handles the events for the Add File Button
+    private class AdminButtonListener implements ActionListener
+    {
+        SearchWindow windowContainingTriggeredControl;
+
+        //TODO : This method should create and open a new admin window using the currentEngine
+        @Override public void actionPerformed(ActionEvent e)
+        {
+
+        }
+
+        public AdminButtonListener(SearchWindow parentWindow)
+        {
+            windowContainingTriggeredControl = parentWindow;
+            return;
+        }
+    }
+
+    //- Handles the events for the Add File Button
+    private class SearchWindowListener extends WindowAdapter
+    {
+        SearchWindow windowContainingTriggeredControl;
+
+        //TODO : This method should perform any save or close operations that need to be done before
+        //- the program closes
+        @Override public void windowClosing(WindowEvent e)
+        {
+            super.windowClosing(e);
+        }
+
+        public SearchWindowListener(SearchWindow parentWindow)
+        {
+            windowContainingTriggeredControl = parentWindow;
+            return;
+        }
+    }
+
+}
